89d9d505b4a94cf37d9a0171427f820678b23b9e
PavloChechegov <paulch1991@gmail.com>
2016-10-31 17:32:45 +0200

    upgrade project, now use only JavaConfig. Refactor code, delete duplicate.

--- src/main/java/com/codetutr/springconfig/WebConfig.java
+++ src/main/java/com/codetutr/springconfig/WebConfig.java
@@ -4,32 +4,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="com.codetutr")
+@ComponentScan(basePackages = "com.codetutr")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	/**
-	 * Since we don't have any controller logic, simpler to just
-	 * define controller for page using View Controller. Note:
-	 * had to extend WebMvcConfigurerAdapter to get this functionality
-	 * @param registry
-	 */
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("home");
-	}
-	
-	@Bean
-	public InternalResourceViewResolver viewResolver() {
-		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-		resolver.setPrefix("/WEB-INF/view/");
-		resolver.setSuffix(".jsp");
-		return resolver;
-	}
-	
+    /**
+     * Since we don't have any controller logic, simpler to just
+     * define controller for page using View Controller. Note:
+     * had to extend WebMvcConfigurerAdapter to get this functionality
+     *
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/view/*").addResourceLocations("/view/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
 }
